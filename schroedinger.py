79706636274084fabcc6534d5ad3a8ee89a8a287
Mohammad Bahmani <bahmani.m.n@gmail.com>
2019-08-21 22:46:58 +0200

    Write the exercise in schroedinger.py via two functions.

--- schroedinger.py
+++ schroedinger.py
@@ -9,203 +9,156 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def load_data(data_dir):
-    """This function reads three files inside the provided directory and assign them
-    to their corresponding numpy arrays
+    """Read in the data from a QM-calculation
     
-    energies = stor the energies from energies.dat file
+    Args: 
+        data_dir: The directory that contains the data.
+        (energies, discrpot.dat, wfuncs.dat)
     
-    coordinate_discrpot = stor the coordinates and potentials from discrpot.dat file
-
-    wfuncs = stor the wave function coefficients from wfuncs.dat file  
-    
+    return: 
+        Tuples (coordinate, energies, potential, wavefunctions) with the coordinates,
+        eigenenergies, potential and wavefunctions on the coordinates.
     """
+    
     energies = np.loadtxt('{}/energies.dat'.format(data_dir))
-    
     coordinate_discrpot = np.loadtxt('{}/discrpot.dat'.format(data_dir))
-    
     wfuncs = np.loadtxt('{}/wfuncs.dat'.format(data_dir))
     
-    print('Here is the array of energies: \n {} \n'.format(energies[:]))
+    coordinate = coordinate_discrpot[:,0]
+    energies = energies[:]
+    potential = coordinate_discrpot[:,1]
+    wavefunctions = wfuncs[:,1:]
+    print('Here is the array of energies: \n {} \n'.format(energies))
+    print('Here is the array of coordinates: \n {} \n'.format(coordinate))
+    print('Here is the array of potentials: \n {} \n'.format(potential))
+    print('Here is the array of potentials: \n {} \n'.format(wavefunctions))
     
-    print('Here is the array of coordinates: \n {} \n'.format(coordinate_discrpot[:,0]))
-    
-    print('Here is the array of potentials: \n {} \n'.format(coordinate_discrpot[:,1]))
-    
-    print('Here is the array of potentials: \n {} \n'.format(wfuncs[:,1:4]))
-    
-    return energies, coordinate_discrpot, wfuncs
+    return coordinate, energies, potential, wavefunctions
 
-# This asks for the directory containing the datas
-#data_dir =input('Please enter the name of the directory containing the data: ')
+def plot_results(plot_title, coordinate, energies, potential, wavefunctions, prefactor=1.0, xlim=None, ylim=None):
+    """Plot the results from a QM-calculation
+    
+    args: 
+        plot_title: Title of the plot
+        coordinate: The coordinate on which the potential and wavefunctions are represented.
+        energiee: Eigenenergies from a QM-calculation
+        potential: Potential values on the coordinates
+        wavefunctions: Wave function values on the coordinates
+        prefactor: Scaling factor for the wave functions (default: 1.0)
+        xlim: Minimal and maximal x-values to show (default: actual plt.xlim values)
+        ylim: Minimal and maximal y-values to show (default: actual plt.ylim values)
+    
+    return: ????
+        Plot the provided data and indicating its title.
+    """
+    plt.title(plot_title, fontsize = 14)
+    plt.xlabel('x [Bohr]', fontsize = 14)
+    plt.ylabel('Energy [Hartree]', fontsize = 14)
+
+    if xlim == None:
+        xmin, xmax = plt.xlim()
+    else:
+        xmin, xmax = xlim
+        plt.xlim(xmin, xmax)
+        
+    if ylim == None:
+        ymin, ymax = plt.ylim()
+    else:   
+        ymin, ymax = ylim        
+        plt.ylim(ymin, ymax)
+
+    plt.plot(coordinate, potential, color = 'black')
+
+    for itr in range(0, np.size(energies)):
+        if itr%2 == 0:
+            plt.plot([xmin, xmax], [energies[itr], energies[itr]], color = 'gray')
+            plt.plot(coordinate, prefactor*wavefunctions[:,itr]+energies[itr], color = 'red')
+        else:
+            plt.plot([xmin, xmax], [energies[itr], energies[itr]], color = 'gray')
+            plt.plot(coordinate, prefactor*wavefunctions[:,itr]+energies[itr], color = 'blue')
+
+#    return???
 
 #  *************************  Inf_well  *************************
-data_dir_infwell = 'schroedinger/inf_well/'
+# Call the function that reads the results
+coordinate, energies, potential, wavefunctions = load_data('schroedinger/inf_well/')
 
-# This loads and print the datas
-energies, coordinate_discrpot, wfuncs =load_data(data_dir_infwell)
-
-print(np.size(coordinate_discrpot[:,0]))
+print(np.size(coordinate))
 print(np.size(energies))
 
 # Start plotting the datas
 plt.figure(figsize = (13,8))
 
-# create a vactor of ones with the dimension of the coordinates.
-# I need it to plot the horizontal gray lines in the plots
-vactor_zeros = np.zeros(np.size(coordinate_discrpot[:,0]), dtype = int) 
-    
+# Set the boundaries on X and Y axis
+xlim = -2.0, 2.0
+ylim = -0.5, 6.5
+
 # Create a new subplot from a grid of 1x2  ***** subplot(1,2,11 *****
 plt.subplot(1,2,1)
-plt.title('Infinite well (wavefunctions)')
 
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+wfuncs[:,2*itr+1]*16, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+wfuncs[:,2*itr+1+1]*16, color = 'blue')
-
-# Set the boundaries on X and Y axis
-plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.ylim(-0.5,6.3)
-
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Call the plotting function
+plot_results('Infinite well (wavefunctions)', coordinate, energies, potential, wavefunctions, prefactor=16, xlim=xlim, ylim=ylim)
 
 # Create a new subplot from a grid of 1x2  ***** subplot(1,2,2) *****
 plt.subplot(1,2,2)
-plt.title('Infinite well (probabilities)')
 
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-    
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+(wfuncs[:,2*itr+1]*wfuncs[:,2*itr+1].transpose())*16*16, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+(wfuncs[:,2*itr+1+1]*wfuncs[:,2*itr+1+1].transpose())*16*16, color = 'blue')
-
-# Set the boundaries on X and Y axis
-plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.ylim(-0.5,6.3)
-
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Call the plotting function
+plot_results('Infinite well (probabilities)', coordinate, energies, potential, np.abs(wavefunctions)**2, prefactor=400, xlim=xlim, ylim=ylim)
 
 plt.show()
 
 #  *************************  Double_well  *************************
-data_dir_doublewell = 'schroedinger/double_well/'
+# Call the function that reads the results
+coordinate, energies, potential, wavefunctions = load_data('schroedinger/double_well/')
 
-# This loads and print the datas
-energies, coordinate_discrpot, wfuncs =load_data(data_dir_doublewell)
-
-print(np.size(coordinate_discrpot[:,0]))
+print(np.size(coordinate))
 print(np.size(energies))
 
 # Start plotting the datas
 plt.figure(figsize = (13,8))
 
-# create a vactor of ones with the dimension of the coordinates.
-# I need it to plot the horizontal gray lines in the plots
-vactor_zeros = np.zeros(np.size(coordinate_discrpot[:,0]), dtype = int) 
-
-# Create a new subplot from a grid of 1x2  ***** subplot(1,2,11 *****
-plt.subplot(1,2,1)
-plt.title('Double well (wavefunctions)')
-
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-    
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+wfuncs[:,2*itr+1]*2, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+wfuncs[:,2*itr+1+1]*2, color = 'blue')
-
 # Set the boundaries on X and Y axis
-#plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.xlim(-12, 12)
-plt.ylim(-1.6,2.5)
+xlim = -12.0, 12.0
+ylim = -1.6, 2.5
 
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Create a new subplot from a grid of 1x2  ***** subplot(1,2,11) *****
+plt.subplot(1,2,1)
+
+# Call the plotting function
+plot_results('Double well (wavefunctions)', coordinate, energies, potential, wavefunctions, prefactor=2.2, xlim=xlim, ylim=ylim)
 
 # Create a new subplot from a grid of 1x2  ***** subplot(1,2,2) *****
 plt.subplot(1,2,2)
-plt.title('Double well (probabilities)')
 
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-    
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+(wfuncs[:,2*itr+1]*wfuncs[:,2*itr+1].transpose())*5*5, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+(wfuncs[:,2*itr+1+1]*wfuncs[:,2*itr+1+1].transpose())*5*5, color = 'blue')
-
-# Set the boundaries on X and Y axis
-#plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.xlim(-12, 12)
-plt.ylim(-1.6,2.5)
-
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Call the plotting function
+plot_results('Double well (probabilities)', coordinate, energies, potential, np.abs(wavefunctions)**2, prefactor=25, xlim=xlim, ylim=ylim)
 
 plt.show()
 
 #  *************************  Harmonic Oscillator  *************************
-data_dir_harmonic = 'schroedinger/harmonic/'
+# Call the function that reads the results
+coordinate, energies, potential, wavefunctions = load_data('schroedinger/harmonic/')
 
-# This loads and print the datas
-energies, coordinate_discrpot, wfuncs =load_data(data_dir_harmonic)
-
-print(np.size(coordinate_discrpot[:,0]))
+print(np.size(coordinate))
 print(np.size(energies))
 
 # Start plotting the datas
 plt.figure(figsize = (13,8))
 
-# create a vactor of ones with the dimension of the coordinates.
-# I need it to plot the horizontal gray lines in the plots
-vactor_zeros = np.zeros(np.size(coordinate_discrpot[:,0]), dtype = int) 
-
-# Create a new subplot from a grid of 1x2  ***** subplot(1,2,11 *****
-plt.subplot(1,2,1)
-plt.title('Harmonic oscillator  (wavefunctions)')
-
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-    
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+wfuncs[:,2*itr+1]*3, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+wfuncs[:,2*itr+1+1]*3, color = 'blue')
-
 # Set the boundaries on X and Y axis
-#plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.xlim(-3, 3)
-plt.ylim(0.0,2.5)
+xlim = -3.0, 3.0
+ylim = 0.0, 2.5
 
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Create a new subplot from a grid of 1x2  ***** subplot(1,2,11) *****
+plt.subplot(1,2,1)
+
+# Call the plotting function
+plot_results('Harmonic oscillator (wavefunctions)', coordinate, energies, potential, wavefunctions, prefactor=2.5, xlim=xlim, ylim=ylim)
 
 # Create a new subplot from a grid of 1x2  ***** subplot(1,2,2) *****
 plt.subplot(1,2,2)
-plt.title('Harmonic oscillator  (probabilities)')
 
-plt.plot(coordinate_discrpot[:,0], coordinate_discrpot[:,1], color = 'black')
-    
-for itr in range(0, int(np.size(energies)/2)):
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr]+vactor_zeros, color = 'gray')
-    plt.plot(coordinate_discrpot[:,0], energies[2*itr+1]+vactor_zeros, color = 'gray')
-    plt.plot(wfuncs[:,0], energies[2*itr]+(wfuncs[:,2*itr+1]*wfuncs[:,2*itr+1].transpose())*6*6, color = 'red')
-    plt.plot(wfuncs[:,0], energies[2*itr+1]+(wfuncs[:,2*itr+1+1]*wfuncs[:,2*itr+1+1].transpose())*6*6, color = 'blue')
-
-# Set the boundaries on X and Y axis
-#plt.xlim((coordinate_discrpot[:,0]).min(), (coordinate_discrpot[:,0]).max())
-plt.xlim(-3, 3)
-plt.ylim(0.0,2.5)
-
-plt.xlabel('x [Bohr]')
-plt.ylabel('Energy [Hartree]')
+# Call the plotting function
+plot_results('Harmonic oscillator (probabilities)', coordinate, energies, potential, np.abs(wavefunctions)**2, prefactor=50, xlim=xlim, ylim=ylim)
 
 plt.show()
