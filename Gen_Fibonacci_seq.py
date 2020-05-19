d81faf5542fd095b7d39b073dbc0fbcefcd56711
Mohammad Bahmani <45482139+MohammadB88@users.noreply.github.com>
2020-05-19 11:19:46 +0200

    Update Gen_Fibonacci_seq.py
    Add generalized Fibonacci with array representation and comments

--- Gen_Fibonacci_seq.py
+++ Gen_Fibonacci_seq.py
@@ -20,28 +20,49 @@ while True:
         break
 
 def Fibonacci_Sequence(inp_num, order=2):
-    elm = []
+    elm_fib = []
 #    print(inp_num,order)
     for N in range(order):    
-        elm.append(1)
-#    print(elm)
+        elm_fib.append(1)
+#    print(elm_fib)
     for i in range(order,inp_num):
-        new_elm = 0
+        fibo = 0
         for N in range(1,order+1):
-            new_elm += elm[i-N] 
-        elm.append(new_elm)
-#    print(elm)
-    return elm
-    
-elm = Fibonacci_Sequence(inp_num, inp_order)
+            fibo += elm_fib[i-N] 
+        elm_fib.append(fibo)
+#    print(elm_fib)
+    return elm_fib
 
-field_width = len(str(abs(elm[inp_num-1])))
+# return an array of Fibonacci numbers
+def Fibonacci_Sequence(inp_num, order=2):
+    """ calculates generalized Fibonacci-numbers
+    
+    Generalized Fibonacci-numbers are obtained by summing up the previous N numbers
+    of the series, where N is the order of the Fibonacci-numbers. The conventional
+    Fibonacci-series can be obtained with N=2.
+    
+    Args:
+        inp_num: Number of terms to calculate.
+        order: Order of Fibonacci-numbers (Optional, default: 2).
+        
+    Returns:
+        List of Fibonacci-numbers.
+    """
+    elm_fib = np.empy(inp_num, dtype=int)
+    elm_fib[0 : min(inp_num, order)] = 1
+    for itm in range(order, inp_num):
+        elm_fib[itm] = np.sum(elm_fib[itm - order: itm])
+    return elm_fib
+
+fibbos = Fibonacci_Sequence(inp_num, inp_order)
+
+field_width = len(str(abs(fibbos[inp_num-1])))
 #print(field_width)
 field_width_format = '{:>'+'{}'.format(field_width)+'}'
 
-#print(elm)
+#print(fibbos)
 if inp_num == 0:
     print(field_width_format.format(0), 'with index number', 1)
 else:
     for i in range(inp_num):
-        print(field_width_format.format(elm[i]), 'with index number', i+1)
+        print(field_width_format.format(fibbos[i]), 'with index number', i+1)
